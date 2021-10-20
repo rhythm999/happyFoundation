351fc5ff8a97860930e30a1f6fcb7f16a461ef7d
Rhythm Sharma <sharmarhythm999@gmail.com>
2021-10-20 10:54:57 +0530

    update

--- script.js
+++ script.js
@@ -5,14 +5,12 @@ donateBtn.addEventListener("click", function(e){
     link.href="https://rzp.io/l/zqi2JDqea";
 });
 var options = {
-    "key": "rzp_test_MWvHEstpsnDD0E", // Enter the Key ID generated from the Dashboard
-    "amount": "50000", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+    "key": "rzp_test_MWvHEstpsnDD0E", 
+    "amount": "50000", 
     "currency": "INR",
     "name": "HAPPY FOUNDATION",
     "description": "DONATE TO MAKE SOMEONE HAPPY",
     "image": "https://example.com/your_logo",
-    // "order_id": "order_Ef80WJDPBmAeNt", //Pass the `id` obtained in the previous step
-    // "account_id": "acc_Ef7ArAsdU5t0XL",
     "handler": function (response) {
         alert(response.razorpay_payment_id);
         alert(response.razorpay_order_id);
